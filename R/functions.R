60b800628f482f96f4e5e99d2965d3337f9d2da3
Alec L. Robitaille <robit.alec@gmail.com>
2021-06-16 15:04:08 -0700

    fix make unique complete and extract lc
    make unique mismatch arg name
    extract fix logic passing lat long

--- R/functions.R
+++ R/functions.R
@@ -6,15 +6,17 @@
 
 # Make unique and complete ------------------------------------------------
 make_unique_complete <- function(DT, id, datetime, long, lat) {
-  na.omit(unique(input, by = c(id, datetime)),
+  na.omit(unique(DT, by = c(id, datetime)),
           cols = c(long, lat, datetime))
 }
 
 
 
 # Extract land cover ------------------------------------------------------
-extract_lc <- function(DT, lc, lcvalues) {
-  merge(DT[, value := extract(lc, xy)], lcvalues, by = value)
+extract_lc <- function(DT, lc, x, y, lcvalues) {
+  merge(DT[, value := raster::extract(lc, matrix(do.call(cbind, .SD), ncol = 2)),
+           .SDcols = c(x, y)],
+        lcvalues, by = value)
 }
 
 
