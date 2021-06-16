2a850eac9ca4061cb9383fdf98458005ce5446e3
Julie Turner <julwturner@gmail.com>
2021-06-16 21:05:49 -0230

    fixing land

--- R/sidetests.R
+++ R/sidetests.R
@@ -1,11 +1,15 @@
 require(targets)
 require(raster)
+require(sf)
 tar_load(inputland)
 tar_load(lcvalues)
 
-road.shp <- 
+land <- st_read(file.path('data', 'raw-data', 'CanLCC.tif'))
 inputland <- projectRaster(inputland, crs = crs)
 writeRaster(inputland, file.path('data', 'raw-data', 'CanLCC_WGS84.tif'))
 
 merge(inputland[, value := extract(lc, xy)], lcvalues, by = value)
+
+
+road.shp <- st_read(file.path('data', 'raw-data', 'sk_roads.shp'))
 
