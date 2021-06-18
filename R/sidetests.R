60df26c771746f9afab8998e9d42b1e912c65ef1
Julie Turner <julwturner@gmail.com>
2021-06-18 10:55:58 -0230

    prepping landscape features

--- R/sidetests.R
+++ R/sidetests.R
@@ -1,15 +1,35 @@
 require(targets)
 require(raster)
 require(sf)
-tar_load(inputland)
-tar_load(lcvalues)
+require(rgeos)
+require(rgdal)
+# tar_load(inputland)
+# tar_load(lcvalues)
 
-land <- st_read(file.path('data', 'raw-data', 'CanLCC.tif'))
-inputland <- projectRaster(inputland, crs = crs)
-writeRaster(inputland, file.path('data', 'raw-data', 'CanLCC_WGS84.tif'))
+crs <- CRS(st_crs(4326)$wkt)
+
+sk<- readRDS(file.path('data', 'derived-data', 'prepped-data', 'SKprepDat.RDS'))
+sk <- sk[complete.cases(long,lat, datetime),.(long, lat, datetime, id)]
+coords<- sk%>%st_as_sf(coords = c('long','lat'))%>%
+  st_set_crs(crs)
+
+land <- raster(file.path('data', 'raw-data', 'CanLCC.tif'))
+projland <- projectRaster(land, crs = crs)
+writeRaster(projland, file.path('data', 'raw-data', 'CanLCC_WGS84.tif'))
 
 merge(inputland[, value := extract(lc, xy)], lcvalues, by = value)
 
 
-road.shp <- st_read(file.path('data', 'raw-data', 'sk_roads.shp'))
+road.shp <- rgdal::readOGR(file.path('data', 'raw-data', 'sk_roads', 'sk_roads.shp'))  
+# %>%
+#    st_transform(crs)# %>% as_Spatial()
+road.shp <- spTransform(road.shp, crs)
+#road.poly <- st_cast(road.shp, 'POLYGON')
+#st_crs(road.shp)
+#dum <- raster(ext = extent(coords))
+dist2road <- gDistance(road.shp, hausdorff = T)
 
+road.ras<- stars::st_rasterize(road.shp)
+road.ras <- raster(road.ras$FID, ext=extent(road.shp))
+
+#plot(road.shp)
