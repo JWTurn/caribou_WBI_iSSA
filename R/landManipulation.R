3c06775206beb28ebf7c729779c8b0c21dbd61ee
Julie Turner <julwturner@gmail.com>
2021-06-18 18:30:42 -0230

    land fights

--- R/landManipulation.R
+++ R/landManipulation.R
@@ -7,6 +7,7 @@ require(rgdal)
 # tar_load(lcvalues)
 
 crs <- CRS(st_crs(4326)$wkt)
+crsSK <- sp::CRS("+init=epsg:2375")
 
 sk<- readRDS(file.path('data', 'derived-data', 'prepped-data', 'SKprepDat.RDS'))
 sk <- sk[complete.cases(long,lat, datetime),.(long, lat, datetime, id)]
@@ -14,9 +15,13 @@ coords<- sk%>%st_as_sf(coords = c('long','lat'))%>%
   st_set_crs(crs)
 
 # did this in Q where I could highlight what I wanted to crop to for now, lost patience
-# land <- raster(file.path('data', 'raw-data', 'CanLCC.tif'))
-# projland <- projectRaster(land, crs = crs)
-# writeRaster(projland, file.path('data', 'raw-data', 'CanLCC_WGS84.tif'))
+land <- raster(file.path('data', 'raw-data', 'CanLCC.tif'))
+WBnad83 <- raster(file.path('data', 'raw-data', 'WB_LCC_NAD83.tif'))
+crpland <- crop(land, extent(WBnad83))
+writeRaster(crpland, file.path('data', 'raw-data', 'WB_LCC_NAD83.tif'))
+
+projland <- projectRaster(crpland, crs = crs)
+writeRaster(projland, file.path('data', 'raw-data', 'CanLCC_WGS84.tif'))
 
 merge(inputland[, value := extract(lc, xy)], lcvalues, by = value)
 
@@ -24,11 +29,11 @@ merge(inputland[, value := extract(lc, xy)], lcvalues, by = value)
 road.shp <- rgdal::readOGR(file.path('data', 'raw-data', 'sk_roads', 'sk_roads.shp'))  
 # %>%
 #    st_transform(crs)# %>% as_Spatial()
-#road.shp <- spTransform(road.shp, crs)
+road.shp <- spTransform(road.shp, crsSK)
 #road.poly <- st_cast(road.shp, 'POLYGON')
 #st_crs(road.shp)
-#dum <- raster(ext = extent(coords))
-dist2road <- gDistance(road.shp, hausdorff = T)
+dum <- raster(ext = extent(road.shp), resolution = 30, crs = crs(road.shp))
+dist2road <- gDistance(road.shp, dum, byid = T)
 
 road.ras<- stars::st_rasterize(road.shp)
 road.ras <- raster(road.ras$FID, ext=extent(road.shp))
