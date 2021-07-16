dfdbc1033f31e1ae57468997ab06c772a00b042c
Julie Turner <julwturner@gmail.com>
2021-07-16 15:37:36 -0230

    tweaks

--- R/prep-nwt.R
+++ R/prep-nwt.R
@@ -1,13 +1,16 @@
 require(Require)
+Require('reproducible')
 Require('move')
 Require('data.table')
 
+cPath <- file.path(tempdir(), "cache")
 loginStored <- movebankLogin(username=Sys.getenv('moveUserID'), 
                              password=Sys.getenv('movePwd'))
 # NWT dataset names to fill in loop
 dsNames <-c('Dehcho Boreal Woodland', 'Inuvik Boreal Woodland', 
             'North Slave Boreal', 'Sahtu Boreal Woodland', 'South Slave Boreal Woodland') 
 # make a list of all data from Movebank
+# TODO: update this with prepInputs()
 nwt.move <- list()
 for (ds in 1:length(dsNames)) {
   #hh = 1
@@ -16,12 +19,16 @@ for (ds in 1:length(dsNames)) {
 }
 
 # pull just the data
-habitat <-c('Dehcho', 'Inuvik', 'North Slave', 'Sahtu', 'South Slave') 
+habitat <-c('dehcho', 'inuvik', 'north.slave', 'sahtu', 'south.slave') 
 
-nwt <- data.table(habitat)
-
+nwt<-data.table()
+nwt <- rbindlist(lapply(1:length(habitat), function(hh){
+  prep_nwt[,.(area=habitat[[hh]], dat=list(setDT(nwt.move[[hh]]@data)))]
+})
+)
 for (hh in 1:length(habitat)) {
-  nwt[[hh]]<-nwt[,dat:=list(setDT(nwt.move[[hh]]@data))]
+  #hh=1
+  
 }
 dat_dehcho <- setDT(dehcho@data)
 dat_dehcho <- dat_dehcho[,.(id=tag_id, herd=habitat, 
