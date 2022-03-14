9a35a9324e8833962a5ed05e5d17ce6039622f93
Julie Turner <julwturner@gmail.com>
2022-03-14 10:59:31 -0230

    making more generalizable

--- R/2-prep-move-data.R
+++ R/2-prep-move-data.R
@@ -4,58 +4,60 @@
 
 
 ### Packages ----
-libs <- c('data.table', 'dplyr', 'amt', 'lubridate', 'raster', 'tidyr', 'ggplot2')
+libs <- c('data.table', 'dplyr', 'amt', 'lubridate', 'terra', 'sf', 'tidyr', 'ggplot2')
 lapply(libs, require, character.only = TRUE)
 
 ### Input data ----
 
-sk <- 'data/derived-data/prepped-data/'
+prepped <- 'data/derived-data/prepped-data/'
 derived <- 'data/derived-data/'
 
-dat_sk <- readRDS(paste0(sk, 'SKprepDat.RDS'))
+#dat <- readRDS(paste0(prepped, 'SKprepDat.RDS'))
+
+dat <- readRDS(paste0(prepped, 'NWTprepDat.RDS'))
 
 
 ### EXPLORE ----
 # check if all observations are complete
-all(complete.cases(dat_sk[,.(long,lat, datetime)]))
+all(complete.cases(dat[,.(x,y, datetime)]))
 # remove incomplete observations
-dat_sk <- dat_sk[complete.cases(long,lat, datetime)]
-all(complete.cases(dat_sk[,.(long,lat, datetime)]))
+dat <- dat[complete.cases(x,y, datetime)]
+all(complete.cases(dat[,.(x,y, datetime)]))
 
 # check for duplicated time stamps
-dat_sk[,any(duplicated(datetime)), by = id]
+dat[,any(duplicated(datetime)), by = id]
 
 # We have some duplicated time stamps, these need to be removed prior to creating a track.
-DT_sk <- unique(dat_sk, by = c('id', 'datetime'))
+DT <- unique(dat, by = c('id', 'datetime'))
 
 
 ### track ####
-trk_sk <- DT_sk %>% make_track(long, lat, datetime, crs = sp::CRS("+init=epsg:4326"), id = id)
-trk_sk <- trk_sk %>% nest(data = -"id")
+trk <- DT %>% make_track(x,y, datetime, crs = st_crs(3978), id = id)
+trk <- trk %>% nest(data = -"id")
 
-fixrate <- trk_sk %>% mutate(sr = lapply(data, summarize_sampling_rate)) %>%
+fixrate <- trk %>% mutate(sr = lapply(data, summarize_sampling_rate)) %>%
   dplyr::select(id, sr) %>% unnest(cols = c(sr))
-# setnames(trk_sk, c('id','data'))
-# trk_sk[, trk:= amt::make_track(data, long, lat, datetime, crs = sp::CRS("+init=epsg:4326")), by = id]
+# setnames(trk, c('id','data'))
+# trk[, trk:= amt::make_track(data, long, lat, datetime, crs = sp::CRS("+init=epsg:4326")), by = id]
 
 
-# trk_sk<- rbindlist(lapply(unique(DT_sk$id), function(i){
-#   DT_sk[id == i, .(amt::make_track(.SD, long, lat, datetime, crs = sp::CRS("+init=epsg:4326"), id = id))]
+# trk<- rbindlist(lapply(unique(DT$id), function(i){
+#   DT[id == i, .(amt::make_track(.SD, long, lat, datetime, crs = sp::CRS("+init=epsg:4326"), id = id))]
 # }))
 ### standardize fix rate ####
 
-#trk_sk2 <-trk_sk %>% nest(data = -"id") 
+#trk2 <-trk %>% nest(data = -"id") 
 # safely gets rid of the individuals causing errors 
 #TODO look into errors later
-trk_sk2 <-trk_sk %>%
+trk2 <-trk %>%
   mutate(steps = map(data, purrr::safely(function(x) 
     x %>% amt::track_resample(rate = hours(5), tolerance = minutes(30)) %>% 
       amt::filter_min_n_burst() %>% amt::steps_by_burst(., lonlat = T))))
 
-steps_by_burst(trk_sk2$steps[[1]], lonlat = T)
+steps_by_burst(trk2$steps[[1]], lonlat = T)
 
 
-trk_sk2 <-trk_sk %>%
+trk2 <-trk %>%
   mutate(steps = map(data, function(x)
     x %>% amt::track_resample(rate = hours(5), tolerance = minutes(30)) %>%
       amt::filter_min_n_burst() %>% amt::steps_by_burst(., lonlat = T)))
@@ -63,7 +65,7 @@ trk_sk2 <-trk_sk %>%
 
 
 ############
-sl<- trk_sk %>%mutate(sl = map(data, function(x) 
+sl<- trk %>%mutate(sl = map(data, function(x) 
   x %>% amt::step_lengths(., lonlat = T)))
 sum <- sl %>%
   mutate(sum = lapply(sl, summary)) %>% 
@@ -71,9 +73,9 @@ sum <- sl %>%
 
 ### look at movement ####
 
-trk_sk3 <- trk_sk2 %>% dplyr::select(id, steps) %>% unnest(cols = steps) 
-test <- data.table(id = trk_sk3$id, trk_sk3$steps$result)
+trk3 <- trk2 %>% dplyr::select(id, steps) %>% unnest(cols = steps) 
+test <- data.table(id = trk3$id, trk3$steps$result)
 ggplot(aes(sl_, fill = factor(id))) + geom_density(alpha = 0.4)
 
-DT_sk %>% ggplot(aes(long,lat, color= factor(id))) +
+DT %>% ggplot(aes(long,lat, color= factor(id))) +
   geom_point()
