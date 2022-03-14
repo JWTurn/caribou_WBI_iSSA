05f7df40e153cfe56b3c0f7f8888f7d3ada76b8a
Julie Turner <julwturner@gmail.com>
2022-03-14 10:59:48 -0230

    adjust habitat categories

--- R/4-iSSA.R
+++ R/4-iSSA.R
@@ -12,11 +12,12 @@ raw <- 'data/raw-data/'
 derived <- 'data/derived-data/'
 
 tar_load(stepID)
+saveRDS(stepID, file.path(derived, 'sk_derived.RDS'))
 
 dat <- stepID
 dat[,id:=as.factor(id)]
 dat[,lc_end_adj := lc_end]
-dat[lc_end %in% c('barren', 'grassland', 'lichen-grass', 'shrub'),lc_end_adj:= 'open-forage']
+dat[lc_end %in% c('barren', 'grassland', 'lichen-grass', 'shrub', 'urban'),lc_end_adj:= 'open-forage']
 dat[lc_end %in% c('deciduous', 'mixedforest'),lc_end_adj:= 'deciduous']
 dat[lc_end %in% c('water', 'wetland'),lc_end_adj:= 'wet']
 
@@ -32,18 +33,16 @@ mod <- glmmTMB(case_ ~
                  I(log(sl_)) +
                  I(log(sl_)):lc_end_adj +
                  lc_end_adj +
-                 dist_lf_end +
-                 I(log(sl_)):dist_lf_end +
+                 I(log(dist_lf_end)) +
                  (1|indiv_step_id) +
                  (0 + I(log(sl_))|id) +
                  (0 + I(log(sl_)):lc_end_adj|id) +
                  (0 + lc_end_adj|id) +
-                 (0 + I(log(sl_)):dist_lf_end|id) +
-                 (0 + dist_lf_end|id),
+                 (0 + I(log(dist_lf_end))|id),
                family = poisson(), data = dat,
-               map= list(theta = factor(c(NA,1:33))), 
-               start = list(theta =c(log(1000), seq(0,0, length.out = 33)))
+               map= list(theta = factor(c(NA,1:22))), 
+               start = list(theta =c(log(1000), seq(0,0, length.out = 22)))
 )
 
 summary(mod)
-saveRDS(mod, file.path(derived, 'toy_ssa.RDS'))
+saveRDS(mod, file.path(derived, 'sk_ssa.RDS'))
