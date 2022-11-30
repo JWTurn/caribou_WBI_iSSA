d99ca877eecea91db5ec04d9293b476a3f2a7454
Julie <julwturner@gmail.com>
2022-11-30 13:53:44 -0500

    fix extract by year for 2021

--- R/extract_by_year.R
+++ R/extract_by_year.R
@@ -2,32 +2,63 @@
 #' @export
 #' @author Julie W. Turner, Alec L. Robitaille
 #' 
-extract_by_year <- function(DT, var, startyr, endyr, step.end  = T){
+extract_by_year <- function(DT, var, startyr, endyr, where = 'end'){
   #lyr <- vect(layer)
   object_name <- deparse(substitute(var))
   yrs <- startyr:endyr
   ls_rast <-c(paste0(var, '_', yrs, '.tif'))
-  names(ls_rast) <- yrs
+  names(ls_rast) <- as.character(yrs)
+  # one year with gps data but no yearly rasters
+  ls_rast['2021'] <- c(paste0(var, '_', '2020', '.tif'))
   
-  if (isTRUE(step.end)) {
-    coords <-  c('x2_', 'y2_')
-    object_end <- 'end'
+  coords_start  <-  c('x1_', 'y1_')
+  coords_end  <-  c('x2_', 'y2_')
+  
+  if (where == 'end') {
+    DT[, paste(object_name, 'end', sep = "_") := terra::extract(
+      rast(ls_rast[as.integer(names(ls_rast)) == .BY[[1]]]),
+      .SD,
+      cells = FALSE,
+      xy = FALSE,
+      ID = FALSE
+    ),
+    by = year,
+    .SDcols = c(coords_end)]
   }
   
-  if(isFALSE(step.end)){
-    coords <-  c('x1_', 'y1_') 
-    object_end <- 'start'
+  if(where == 'start'){
+    DT[, paste(object_name, 'start', sep = "_") := terra::extract(
+      rast(ls_rast[as.integer(names(ls_rast)) == .BY[[1]]]),
+      .SD,
+      cells = FALSE,
+      xy = FALSE,
+      ID = FALSE
+    ),
+    by = year,
+    .SDcols = c(coords_start)]
   }
   
-  DT[, paste(object_name, object_end, sep = "_") := extract(
-    rast(ls_rast[as.integer(names(ls_rast)) == .BY[[1]]]),
-    .SD,
-    cells = FALSE,
-    xy = FALSE,
-    ID = FALSE
-  ),
-  by = year,
-  .SDcols = c(coords)]
-
+  
+  if(where == 'both'){
+    DT[, paste(object_name, 'start', sep = "_") := terra::extract(
+      rast(ls_rast[as.integer(names(ls_rast)) == .BY[[1]]]),
+      .SD,
+      cells = FALSE,
+      xy = FALSE,
+      ID = FALSE
+    ),
+    by = year,
+    .SDcols = c(coords_start)]
+    
+    DT[, paste(object_name, 'end', sep = "_") := terra::extract(
+      rast(ls_rast[as.integer(names(ls_rast)) == .BY[[1]]]),
+      .SD,
+      cells = FALSE,
+      xy = FALSE,
+      ID = FALSE
+    ),
+    by = year,
+    .SDcols = c(coords_end)]
+  }
   
 }
