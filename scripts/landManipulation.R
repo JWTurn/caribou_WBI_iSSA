fff9d1719f4656ee6ea39ff4c419817147d459ee
Julie <julwturner@gmail.com>
2022-12-02 11:03:05 -0500

    saving prop landcover rasters

--- scripts/landManipulation.R
+++ scripts/landManipulation.R
@@ -52,38 +52,49 @@ names(snow) <- "snow"
 ## This creates an object which can be used to make a layer of specified diameter
 # The d value is what determines the buffer size if you want to change it.
 ## If you're doing multiple landcover classes, you only need to run this line once, as long as each of the habitat variables has the same resolution
-# (e.g., the "Wetland" here could be any cover type)
 Buff <- focalMat(land, d=buff.diam, type = 'circle')
-## This generates a new raster where each cell corresponds to the mean wetland within a 100m buffer.
+## This generates a new raster where each cell corresponds to the mean wetland within the buffer.
 # Since it's all 1s and 0s, this is the same as the proportion of wetland surrounding the focal variable
 propneedle <- focal(needleleaf, Buff, na.rm = TRUE, pad = TRUE, padValue = 0)
-propdecid <- focal(deciduous, Buff, na.rm = TRUE, pad = TRUE, padValue = 0)
-propmixed <- focal(mixed, Buff, na.rm = TRUE, pad = TRUE, padValue = 0)
-propshrub <- focal(shrub, Buff, na.rm = TRUE, pad = TRUE, padValue = 0)
-propgrass <- focal(grassland, Buff, na.rm = TRUE, pad = TRUE, padValue = 0)
-proplichshrub <- focal(lichenshrub, Buff, na.rm = TRUE, pad = TRUE, padValue = 0)
-proplichgrass <- focal(lichengrass, Buff, na.rm = TRUE, pad = TRUE, padValue = 0)
-propwet <- focal(wet, Buff, na.rm = TRUE, pad = TRUE, padValue = 0)
-propcrop <- focal(cropland, Buff, na.rm = TRUE, pad = TRUE, padValue = 0)
-propbarren <- focal(barrenland, Buff, na.rm = TRUE, pad = TRUE, padValue = 0)
-propurban <- focal(urban, Buff, na.rm = TRUE, pad = TRUE, padValue = 0)
-propwater <- focal(water, Buff, na.rm = TRUE, pad = TRUE, padValue = 0)
-propsnow <- focal(snow, Buff, na.rm = TRUE, pad = TRUE, padValue = 0)
-
 writeRaster(propneedle, file.path('data', 'raw-data', 'prop_needleleaf.tif'))
+
+propdecid <- focal(deciduous, Buff, na.rm = TRUE, pad = TRUE, padValue = 0)
 writeRaster(propdecid, file.path('data', 'raw-data', 'prop_deciduous.tif'))
+
+propmixed <- focal(mixed, Buff, na.rm = TRUE, pad = TRUE, padValue = 0)
 writeRaster(propmixed, file.path('data', 'raw-data', 'prop_mixed.tif'))
+
+propshrub <- focal(shrub, Buff, na.rm = TRUE, pad = TRUE, padValue = 0)
 writeRaster(propshrub, file.path('data', 'raw-data', 'prop_shrub.tif'))
+
+propgrass <- focal(grassland, Buff, na.rm = TRUE, pad = TRUE, padValue = 0)
 writeRaster(propgrass, file.path('data', 'raw-data', 'prop_grassland.tif'))
+
+proplichshrub <- focal(lichenshrub, Buff, na.rm = TRUE, pad = TRUE, padValue = 0)
 writeRaster(proplichshrub, file.path('data', 'raw-data', 'prop_lichenshrub.tif'))
+
+proplichgrass <- focal(lichengrass, Buff, na.rm = TRUE, pad = TRUE, padValue = 0)
 writeRaster(proplichgrass, file.path('data', 'raw-data', 'prop_lichengrass.tif'))
+
+propwet <- focal(wet, Buff, na.rm = TRUE, pad = TRUE, padValue = 0)
 writeRaster(propwet, file.path('data', 'raw-data', 'prop_wetland.tif'))
+
+propcrop <- focal(cropland, Buff, na.rm = TRUE, pad = TRUE, padValue = 0)
 writeRaster(propcrop, file.path('data', 'raw-data', 'prop_cropland.tif'))
+
+propbarren <- focal(barrenland, Buff, na.rm = TRUE, pad = TRUE, padValue = 0)
 writeRaster(propbarren, file.path('data', 'raw-data', 'prop_barrenland.tif'))
+
+propurban <- focal(urban, Buff, na.rm = TRUE, pad = TRUE, padValue = 0)
 writeRaster(propurban, file.path('data', 'raw-data', 'prop_urban.tif'))
+
+propwater <- focal(water, Buff, na.rm = TRUE, pad = TRUE, padValue = 0)
 writeRaster(propwater, file.path('data', 'raw-data', 'prop_water.tif'))
+
+propsnow <- focal(snow, Buff, na.rm = TRUE, pad = TRUE, padValue = 0)
 writeRaster(propsnow, file.path('data', 'raw-data', 'prop_snow.tif'))
 
+#######S
 
 roads <- st_read(file.path('data', 'raw-data', 'wbi_nrn.shp'))
 rail <- st_read(file.path('data', 'raw-data', 'wbi_rail.shp'))
