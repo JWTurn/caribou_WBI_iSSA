bd44ed896cd4279f5445890773aafd4b9183b692
Julie <julwturner@gmail.com>
2023-01-23 15:45:27 -0600

    get rid of typo extract_distto
    oy veh, are all my problems because I can't type?

--- R/extract_distto.R
+++ R/extract_distto.R
@@ -9,19 +9,19 @@ extract_distto <- function(DT, feature, where = 'end', crs) {
   coords_end  <-  c('x2_', 'y2_')
   
   if(where == 'start'){
-    DT[, paste0('dist', oject_name, '_start') := distance_to(st_as_sf(.SD, coords = coords_start,
+    DT[, paste0('dist', object_name, '_start') := distance_to(st_as_sf(.SD, coords = coords_start,
                                                               crs = crs), feature)]
   }
   
   if(where == 'end'){
-    DT[, paste0('dist', oject_name, '_end') := distance_to(st_as_sf(.SD, coords = coords_end,
+    DT[, paste0('dist', object_name, '_end') := distance_to(st_as_sf(.SD, coords = coords_end,
                                                                       crs = crs), feature)]
   }
   
   if(where == 'both'){
-    DT[, paste0('dist', oject_name, '_start') := distance_to(st_as_sf(.SD, coords = coords_start,
+    DT[, paste0('dist', object_name, '_start') := distance_to(st_as_sf(.SD, coords = coords_start,
                                                                       crs = crs), feature)]
-    DT[, paste0('dist', oject_name, '_end') := distance_to(st_as_sf(.SD, coords = coords_end,
+    DT[, paste0('dist', object_name, '_end') := distance_to(st_as_sf(.SD, coords = coords_end,
                                                                    crs = crs), feature)]
   }
   
