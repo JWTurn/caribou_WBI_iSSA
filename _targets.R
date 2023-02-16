f560a20596c8a27d87b03c1b6e7c849087be53f4
Julie <julwturner@gmail.com>
2023-02-16 10:16:22 -0600

    update targets w/ quick fixes for extract prop layers

--- _targets.R
+++ _targets.R
@@ -15,6 +15,9 @@ library(sp)
 library(ggplot2)
 library(glmmTMB)
 library(distanceto)
+library(dtplyr)
+library(dplyr, warn.conflicts = FALSE)
+
 #library(landscapemetrics)
 
 # Functions ---------------------------------------------------------------
@@ -62,10 +65,15 @@ can2020 <- file.path(canada, 'canada_2020', 'landcover-2020-classification.tif')
 #   r_path = landvals
 # )
 # values$raster_name <- basename(xfun::sans_ext(values$r_path))
+# saveRDS(values, file.path('data', 'raw-data', 'prop_land', year,'propvalues.RDS'))
 # values$raster_name_sym <- lapply(values$raster_name, as.symbol)
 # # Extraction
 # values$extract_name_sym <- lapply(paste0('extract_', values$raster_name), as.symbol)
 
+## this is a short term crutch to get it to work
+values2010 <- file.path('data', 'raw-data', 'prop_land', '2010','propvalues.RDS')
+values2015 <- file.path('data', 'raw-data', 'prop_land', '2015','propvalues.RDS')
+values2020 <- file.path('data', 'raw-data', 'prop_land', '2020','propvalues.RDS')
 
 linfeat <- file.path('data', 'raw-data', 'wbi_roads.shp')
 fires <- file.path('data', 'raw-data', 'fire_nbac_1986_to_2020', 'fires')
@@ -76,7 +84,7 @@ longlat = FALSE
 #not actually longitude and latitude, just don't want to change code
 long <- 'x'
 lat <- 'y'
-crs <- CRS(st_crs(3978)$wkt)
+crs <- st_crs(3978)$wkt
 
 # minimum year we want to pull data for
 minyr <- 2010
@@ -177,12 +185,9 @@ targets_tracks <- c(
     distparams,
     calc_distribution_parameters(randsteps),
     pattern = map(randsteps)
-  )
-)
-
-# Targets: extract ------------------------------------------------------------------
-targets_fires <- c(    
-  # make a data.table for future manipulations
+  ), 
+  
+  # make a data.table so easier to manipulate
   tar_target(
     dattab,
     make_data_table(randsteps)
@@ -193,9 +198,70 @@ targets_fires <- c(
     addyear,
     dattab[,`:=`(year=lubridate::year(t2_), 
                  int.year=plyr::round_any(lubridate::year(t2_), interval, floor))]
+  )
+)
+
+
+# Targets: extract ------------------------------------------------------------------
+# Targets: extract proportion of land
+targets_propland2010 <- c(
+  tar_map(
+    readRDS(values2010),
+    tar_target(extract2010,
+               extract_pt(addyear, r_path, raster_name, where = 'both', int.yr = 2010)),
+    unlist = FALSE)
+  )
+  
+targets_propland2015 <- c(  
+  tar_map(
+    readRDS(values2015),
+    tar_target(extract2015,
+               extract_pt(addyear, r_path, raster_name, where = 'both', int.yr = 2015)),
+    unlist = FALSE)
+  )
+  
+targets_propland2020 <- c(  
+  tar_map(
+    readRDS(values2020),
+    tar_target(extract2020,
+               extract_pt(addyear, r_path, raster_name, where = 'both', int.yr = 2020)),
+    unlist = FALSE)
+)
+
+
+targets_proplandcombo_year<- c(
+  tar_combine(
+    propland2010,
+    targets_propland2010,
+    command =  list(!!!.x) %>% purrr::reduce(dplyr::full_join, by = names(addyear))
   ),
   
 
+    tar_combine(
+    propland2015,
+    targets_propland2015,
+    command = list(!!!.x) %>% purrr::reduce(dplyr::full_join, by = names(addyear))
+  ),
+  
+
+    tar_combine(
+    propland2020,
+    targets_propland2020,
+    command = list(!!!.x) %>% purrr::reduce(dplyr::full_join, by = names(addyear))
+  )
+)
+  
+  
+targets_proplandcombo <- c(  
+  tar_combine(
+    extractprop,
+    list(targets_proplandcombo_year),
+    command = dplyr::bind_rows(!!!.x)
+  )
+)
+
+targets_fires <- c(    
+  
   # Extract fires
   tar_target(
     extrfires,
@@ -205,97 +271,53 @@ targets_fires <- c(
   # calculate time since fire
   tar_target(
     tsfire,
-    calc_tsf(extrfires, where = 'both', nofire=100)
-  )
-  
-  # # Calculate distance to linear features
-  # tar_target(
-  #   disttolf,
-  #   extract_distto(tsfire, lf, where = 'both', crs)
-  # )
-  # 
-  # # Set up split -- these are our iteration units
-  # tar_target(
-  #   yrsplits,
-  #   disttolf[, tar_groupyr := .GRP, by = 'int.year'],
-  #   iteration = 'group'
-  # ),
-  # 
-  # # extract proportion of land types
-  # tar_target(
-  #   extrland,
-  #   extract_proportion(yrsplits, feature = land, landclass, buff = buffer, crs, where = 'both'),
-  #   pattern = map(yrsplits)
-  # ),
-  # 
-  # # create step ID across individuals
-  # tar_target(
-  #   stepID,
-  #   setDT(extrland)[,indiv_step_id := paste(id, step_id_, sep = '_')]
-  # )
+    calc_tsf(extrfires, where = 'both', nofire=100) 
+    )
+)
+ 
+targets_distto <- c(     
+    # Calculate distance to linear features
+    tar_target(
+      disttolf,
+      extract_distto(tsfire, lf, where = 'both', crs)
+    )
   
 )
 
-targets_proportions <- c(
-  # calculate buffer
-  tar_target(
-    buffer,
-    plyr::round_any(median(dattab$sl_, na.rm = T), 50, floor)
-  ),
-  
-  # make proportion rasters
-  tar_target(
-    prop2010,
-    make_landsat_prop(layer = can2010, studyArea, crs, buff = buffer, year = 2010)
-  ),
-  
-  tar_target(
-    prop2015,
-    make_landsat_prop(layer = can2015, studyArea, crs, buff = buffer, year = 2015)
-  ),
-  
-  tar_target(
-    prop2020,
-    make_landsat_prop(layer = can2020, studyArea, crs, buff = buffer, year = 2020)
-  )
-  
-)
-
-targets_land <- c(
-  
-  # Calculate distance to linear features
-  tar_target(
-    disttolf,
-    extract_distto(tsfire, lf, where = 'both', crs)
-  ),
-
-  tar_map(
-    prop2010,
-    tar_target(extract,
-               extract_pt(disttolf, r_path, r_name, where = 'both', out = 'new', int.yr = 2010)),
-    unlist = FALSE),
-
-  tar_map(
-    prop2015,
-    tar_target(extract,
-               extract_pt(disttolf, r_path, r_name, where = 'both', out = 'new', int.yr = 2015)),
-    unlist = FALSE),
-  
-  tar_map(
-    prop2020,
-    tar_target(extract,
-               extract_pt(disttolf, r_path, r_name, where = 'both', out = 'new', int.yr = 2020)),
-    unlist = FALSE)
-
-)
+# targets_proportions <- c(
+#   # calculate buffer
+#   tar_target(
+#     buffer,
+#     plyr::round_any(median(dattab$sl_, na.rm = T), 50, floor)
+#   ),
+#   
+#   # make proportion rasters
+#   tar_target(
+#     prop2010,
+#     make_landsat_prop(layer = can2010, studyArea, crs, buff = buffer, year = 2010)
+#   ),
+#   
+#   tar_target(
+#     prop2015,
+#     make_landsat_prop(layer = can2015, studyArea, crs, buff = buffer, year = 2015)
+#   ),
+#   
+#   tar_target(
+#     prop2020,
+#     make_landsat_prop(layer = can2020, studyArea, crs, buff = buffer, year = 2020)
+#   )
+#   
+# )
 
 
-Targets: combine ------------------------------------------------------------------
-targets_combine <- c(
+
+# Targets: combine ------------------------------------------------------------------
+
+targets_combine <- c(  
   tar_combine(
     extrland,
-    list(targets_land),
-    command = dplyr::bind_cols(!!!.x)
+    targets_distto, targets_proplandcombo,
+    command = list(!!!.x) %>% purrr::reduce(dplyr::full_join, by = names(addyear))
   ),
 
 
