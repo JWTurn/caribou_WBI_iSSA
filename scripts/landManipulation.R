1c072a4c05d1e195e1a9921f75c96739bc19de74
Julie <julwturner@gmail.com>
2023-02-28 14:43:01 -0600

    prep harv and ECCC disturbance layers

--- scripts/landManipulation.R
+++ scripts/landManipulation.R
@@ -37,6 +37,8 @@ coords <- st_read(file.path('data', 'derived-data', 'prepped-data', 'WBIprepDat.
 #land <- rast(file.path('data', 'raw-data', 'WB_LC.tif'))
 
 studyArea <- vect(file.path('data', 'derived-data', 'prepped-data', 'WBIprepDat_10kmBuff.shp'))
+
+## LAND ----
 can2010 <- rast(file.path(canada, 'canada_2010', 'CAN_LC_2010_CAL.tif'))
 can2015 <- rast(file.path(canada, 'canada_2015', 'CAN_LC_2015_CAL.tif'))
 can2020 <- rast(file.path(canada, 'canada_2020', 'landcover-2020-classification.tif'))
@@ -56,7 +58,7 @@ wb2010 <- crop(can2010, studyArea)
 #plot(wb2010)
 writeRaster(wb2010, file.path('data', 'derived-data', 'prepped-data', 'WB_LC_2010.tif'))
 
-
+## PROP LANDCOVER ----
 # What to buffer for proportion of landclasses
 buff.diam <- 500  ## median step length = 752, I chose something a bit less
 
@@ -131,7 +133,7 @@ writeRaster(propwater, file.path('data', 'raw-data', 'prop_water.tif'))
 propsnow <- focal(snow, Buff, na.rm = TRUE, pad = TRUE, padValue = 0)
 writeRaster(propsnow, file.path('data', 'raw-data', 'prop_snow.tif'))
 
-#######S
+## ROADS ----
 
 roads <- st_read(file.path('data', 'raw-data', 'wbi_nrn.shp'))
 rail <- st_read(file.path('data', 'raw-data', 'wbi_rail.shp'))
@@ -139,7 +141,75 @@ rail <- st_read(file.path('data', 'raw-data', 'wbi_rail.shp'))
 paved <- dplyr::filter(roads, PAVSTATUS %in% 'Paved')
 #writeVector(vect(paved), file.path('data', 'raw-data', 'wbi_pave_roads.shp'), overwrite = T)
 
- #### FIRES #####
+### ECCC disturbance ----
+eccc_lines_2010 <- vect(file.path(canada, 'caribou-disturbance', '2010', 
+                                  'Boreal-ecosystem-anthropogenic-disturbance-vector-data-2008-2010', 
+                                  'EC_borealdisturbance_linear_2008_2010_FINAL_ALBERS.shp'))
+eccc_lines_2010 <- project(eccc_lines_2010, studyArea)
+
+#eccc_lines_2010.crop <- crop(eccc_lines_2010, ext(studyArea))
+
+eccc_polys_2010 <- vect(file.path(canada, 'caribou-disturbance', '2010', 
+                                  'Boreal-ecosystem-anthropogenic-disturbance-vector-data-2008-2010', 
+                                  'EC_borealdisturbance_polygonal_2008_2010_FINAL_ALBERS.shp'))
+eccc_polys_2010 <- project(eccc_polys_2010, studyArea)
+#eccc_polys_2010.crop <- crop(eccc_polys_2010, ext(studyArea))
+
+
+eccc_lines_2015 <- vect(file.path(raw, 'ECCC_disturbance', 'WB_dist_2015_line.shp'))
+eccc_lines_2015 <- project(eccc_lines_2015, studyArea)
+#eccc_lines_2015.crop <- crop(eccc_lines_2015, ext(studyArea))
+
+eccc_polys_2015 <- vect(file.path(raw, 'ECCC_disturbance', 'WB_dist_2015_poly.shp'))
+eccc_polys_2015 <- project(eccc_polys_2015, studyArea)
+
+summary(as.factor(eccc_lines_2010$Class))
+notroads_2010 <- subset(eccc_lines_2010, !(eccc_lines_2010$Class %in% c('Road', 'Railway')))
+
+
+summary(as.factor(eccc_lines_2015$Class))
+notroads_2015 <- subset(eccc_lines_2015, !(eccc_lines_2015$Class %in% c('Road', 'Railway')))
+
+notroads_2010.crop <- crop(notroads_2010, ext(notroads_2015))
+
+summary(as.factor(eccc_polys_2010$Class))
+disturb_2010 <- subset(eccc_polys_2010, !(eccc_polys_2010$Class %in% c('Cutblock')))
+
+summary(as.factor(eccc_polys_2015$Class))
+disturb_2015 <- subset(eccc_polys_2015, !(eccc_polys_2015$Class %in% c('Cutblock')))
+
+disturb_2010.crop <- crop(disturb_2010, ext(disturb_2015))
+
+dist.mask <- mask(disturb_2010.crop, disturb_2015, inverse = T)
+disturb_2015_merge <- union(disturb_2015, dist.mask)
+
+
+writeVector(notroads_2010.crop, file.path(raw, 'ECCC_disturbance', 'WB_lfother_2010.shp'))
+writeVector(notroads_2015, file.path(raw, 'ECCC_disturbance', 'WB_lfother_2015.shp'))
+
+writeVector(disturb_2010.crop, file.path(raw, 'ECCC_disturbance', 'WB_disturb_other_2010.shp'))
+writeVector(disturb_2015_merge, file.path(raw, 'ECCC_disturbance', 'WB_disturb_other_2015.shp'))
+
+land <- rast(file.path('data', 'raw-data','prop_land', 2010, 'prop_deciduous.tif'))
+disturb_2010_rast <- rasterize((vect(file.path(raw, 'ECCC_disturbance', 'WB_disturb_other_2010.shp'))),
+                               land)
+disturb_2010_rast[is.na(disturb_2010_rast)]<-0
+
+disturb_2015_rast <- rasterize((vect(file.path(raw, 'ECCC_disturbance', 'WB_disturb_other_2015.shp'))),
+                               land)
+disturb_2015_rast[is.na(disturb_2015_rast)]<-0
+
+writeRaster(disturb_2010_rast, file.path(raw, 'ECCC_disturbance', 'WB_disturb_other_2010.tif'))
+writeRaster(disturb_2015_rast, file.path(raw, 'ECCC_disturbance', 'WB_disturb_other_2015.tif'))
+
+## HARVEST ----
+harv <- rast(file.path(canada, 'CA_Forest_Harvest_1985-2020', 'CA_Forest_Harvest_1985-2020.tif'))
+harv <- project(harv, crs, method = 'near')
+harv.crop <- crop(harv, ext(disturb_2015))
+harv.crop[harv.crop==0]<-NA
+writeRaster(harv.crop, file.path(raw, 'WB_harv_1985-2020.tif'), overwrite =T)
+
+## FIRES #####
 ## this has been run already
 # fires <- vect(file.path('data', 'raw-data', 'fire_nbac_1986_to_2020', 'nbac_1986_to_2020_20210810.shp'))
 # fires.proj <- project(fires, crs)
