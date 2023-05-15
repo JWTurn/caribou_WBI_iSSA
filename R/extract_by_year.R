a6e9e62c7c13fbf078ed92e8ccf5957a078d0448
Julie <julwturner@gmail.com>
2023-05-15 16:39:59 -0600

    make extract_by_year more elegant

--- R/extract_by_year.R
+++ R/extract_by_year.R
@@ -2,14 +2,21 @@
 #' @export
 #' @author Julie W. Turner, Alec L. Robitaille
 #' 
-extract_by_year <- function(DT, var, startyr, endyr, where = 'end', out = NULL){
+extract_by_year <- function(DT, var, startyr, endyr, maxyr, where = 'end', out = NULL){
   #lyr <- vect(layer)
   object_name <- deparse(substitute(var))
   yrs <- startyr:endyr
   ls_rast <-c(paste0(var, '_', yrs, '.tif'))
   names(ls_rast) <- as.character(yrs)
   # one year with gps data but no yearly rasters
-  ls_rast['2021'] <- c(paste0(var, '_', '2020', '.tif'))
+  if(endyr < maxyr){
+    newyrs <- endyr:maxyr
+    for (yy in newyrs) {
+      ls_rast[newyrs[yy]] <- c(paste0(var, '_', endyr, '.tif'))
+    }
+    
+  }
+  
   
   coords_start  <-  c('x1_', 'y1_')
   coords_end  <-  c('x2_', 'y2_')
