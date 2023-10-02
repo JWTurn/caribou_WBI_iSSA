060a110412ec5a43b212882d768d2dbe275a5b8c
Julie Turner <julwturner@gmail.com>
2023-10-02 17:10:53 -0600

    edits

--- scripts/4-iSSA.R
+++ scripts/4-iSSA.R
@@ -16,108 +16,110 @@ derived <- file.path('data', 'derived-data')
 # prepped <- readRDS(file.path('data', 'derived-data', 'prepped-data', 'WBIprepDat.RDS'))
 # meta <- unique(prepped[,.(id, jurisdiction, pop=tolower(gsub(' ', '.',pop)), subpop)])
 
-dat.derive <- tar_read(stepID)
+#dat.derive <- tar_read(stepID)
 # dat <- merge(dat, meta, by = 'id', all.x = T)
+dat <- readRDS(file.path(derived, 'dat_iSSA.RDS'))
 
 
-# remove tracks that couldn't estimate movement parameters at this fix rate
-dat.steps <- dat.derive[!is.na(x2_) & !is.na(y2_),.(n = .N), by = .(indiv_step_id)]
-
-dat <- dat.derive[indiv_step_id %in% dat.steps[n ==11, .(indiv_step_id)]$indiv_step_id]
-
-# Check availability
-dat[,.(water = mean(prop_water_end, na.rm = T), snow = mean(prop_snow_end, na.rm = T),
-       rock = mean(prop_rock_end, na.rm = T), barren = mean(prop_barren_end, na.rm = T),
-       bryoids = mean(prop_bryoids_end, na.rm = T), shrub = mean(prop_shrub_end, na.rm = T),
-       wetland = mean(prop_wetland_end, na.rm = T), wettreed = mean(prop_wettreed_end, na.rm = T), 
-       herbs = mean(prop_herbs_end, na.rm = T), needle =mean(prop_needleleaf_end, na.rm = T),
-       decid = mean(prop_deciduous_end, na.rm = T),mixed = mean(prop_mixed_end, na.rm = T)), 
-    by = .(int.year, jurisdiction)]
-# snow, rock, barren, bryoids, generally below 1% availability
-# dominant: needle, wetland
-
-# aggregate habitat types
-dat[, prop_mixforest_start := prop_deciduous_start + prop_mixed_start + prop_wettreed_start]
-dat[, prop_veg_start := prop_shrub_start + prop_bryoids_start + prop_herbs_start]
-dat[, prop_wets_start := prop_wetland_start]
-dat[, prop_openbarren_start := prop_rock_start + prop_barren_start + prop_water_start]
-dat[, prop_open_start := prop_rock_start + prop_barren_start + prop_bryoids_start + prop_herbs_start + prop_water_start]
-dat[, prop_otherveg_start := prop_deciduous_start + prop_mixed_start + prop_wettreed_start +
-      prop_shrub_start + prop_bryoids_start + prop_herbs_start]
-
-dat[, prop_mixforest_end := prop_deciduous_end + prop_mixed_end + prop_wettreed_end]
-dat[, prop_veg_end := prop_shrub_end + prop_bryoids_end + prop_herbs_end]
-dat[, prop_wets_end := prop_wetland_end] 
-dat[, prop_openbarren_end := prop_rock_end + prop_barren_end + prop_water_end]
-dat[, prop_open_end := prop_rock_end + prop_barren_end + prop_bryoids_end + prop_herbs_end + prop_water_end]
-dat[, prop_otherveg_end := prop_deciduous_end + prop_mixed_end + prop_wettreed_end +
-      prop_shrub_end + prop_bryoids_end + prop_herbs_end]
-
-# TODO should wettreed be in wets or mixforest?
-# not include snow
-
-## this was for Canada LCC (landsat)
-# dat[,.(needle =mean(prop_needleleaf_end, na.rm = T), decid = mean(prop_deciduous_end, na.rm = T),
-#           mixed = mean(prop_mixed_end, na.rm = T), shrub = mean(prop_shrub_end, na.rm = T),
-#           grass = mean(prop_grassland_end, na.rm = T), lichshrub = mean(prop_lichenshrub_end, na.rm = T),
-#           lichgrass = mean(prop_lichengrass_end, na.rm = T), crop = mean(prop_cropland_end, na.rm = T),
-#           wetland = mean(prop_wetland_end, na.rm = T), barren = mean(prop_barrenland_end, na.rm = T),
-#           urban = mean(prop_urban_end, na.rm = T), water = mean(prop_water_end, na.rm = T),
-#        snow = mean(prop_snow_end, na.rm = T)), by = .(int.year, jurisdiction)]
-# not include urban or cropland
-# dat[, prop_forest_start := prop_needleleaf_start + prop_deciduous_start + prop_mixed_start]
-# dat[, prop_forage_start := prop_shrub_start + prop_grassland_start + prop_lichenshrub_start + prop_lichengrass_start]
-# dat[, prop_open_start := prop_barrenland_start + prop_snow_start]
-# dat[, prop_wets_start := prop_wetland_start + prop_water_start]
 # 
-# dat[, prop_forest_end := prop_needleleaf_end + prop_deciduous_end + prop_mixed_end]
-# dat[, prop_forage_end := prop_shrub_end + prop_grassland_end + prop_lichenshrub_end + prop_lichengrass_end]
-# dat[, prop_open_end := prop_barrenland_end + prop_snow_end]
-# dat[, prop_wets_end := prop_wetland_end + prop_water_end]
-
-dat[,.(needle =mean(prop_needleleaf_end, na.rm = T), mixforest =mean(prop_mixforest_end, na.rm = T), veg = mean(prop_veg_end, na.rm = T),
-       open = mean(prop_open_end, na.rm = T), wets = mean(prop_wets_end, na.rm = T),
-       openbarren = mean(prop_openbarren_end, na.rm = T), otherveg = mean(prop_otherveg_end, na.rm = T)), 
-    by = .(int.year, jurisdiction)]
-
-dat[,.(distlf_end =mean(distlf_end, na.rm = T), distlf_other_end = mean(distlf_other_end, na.rm = T),
-       disturbance = mean(disturbance_end, na.rm = T), tsf = mean(ts_fires_end, na.rm = T),
-       tsh = mean(ts_harv_end, na.rm = T)), 
-    by = .(int.year, jurisdiction)]
-## prop_open too low availability for most
-
-
-# change arbitrary time since variable
-hist(dat[case_==TRUE]$ts_harv_end)
-hist(dat[case_==FALSE]$ts_harv_end)
-hist(dat[case_==TRUE & jurisdiction == 'mb']$ts_harv_end)
-hist(dat[case_==FALSE & jurisdiction == 'mb']$ts_harv_end)
-ggplot(data = dat, aes(case_, ts_harv_end, color = jurisdiction)) +
-  geom_jitter() +
-  geom_boxplot() +
-  facet_wrap(~int.year)
-
-hist(dat[case_==TRUE]$ts_fires_end)
-hist(dat[case_==FALSE]$ts_fires_end)
-hist(dat[case_==TRUE & jurisdiction == 'mb']$ts_fires_end)
-hist(dat[case_==FALSE & jurisdiction == 'mb']$ts_fires_end)
-ggplot(data = dat, aes(case_, ts_fires_end, color = jurisdiction)) +
-  geom_jitter() +
-  geom_boxplot() +
-  facet_wrap(~int.year)
-
-quantile(dat$ts_harv_end)
-dat[ts_harv_start == 100, ts_harv_start:= 40]
-dat[ts_harv_end == 100, ts_harv_end:= 40]
-range(dat$ts_harv_end)
-quantile(dat$ts_harv_end)
-
-quantile(dat$ts_fires_end)
-dat[ts_fires_start == 100, ts_fires_start:= 40]
-dat[ts_fires_end == 100, ts_fires_end:= 40]
-range(dat$ts_fires_end)
-quantile(dat$ts_fires_end)
-
+# # remove tracks that couldn't estimate movement parameters at this fix rate
+# dat.steps <- dat.derive[!is.na(x2_) & !is.na(y2_),.(n = .N), by = .(indiv_step_id)]
+# 
+# dat <- dat.derive[indiv_step_id %in% dat.steps[n ==11, .(indiv_step_id)]$indiv_step_id]
+# 
+# # Check availability
+# dat[,.(water = mean(prop_water_end, na.rm = T), snow = mean(prop_snow_end, na.rm = T),
+#        rock = mean(prop_rock_end, na.rm = T), barren = mean(prop_barren_end, na.rm = T),
+#        bryoids = mean(prop_bryoids_end, na.rm = T), shrub = mean(prop_shrub_end, na.rm = T),
+#        wetland = mean(prop_wetland_end, na.rm = T), wettreed = mean(prop_wettreed_end, na.rm = T), 
+#        herbs = mean(prop_herbs_end, na.rm = T), needle =mean(prop_needleleaf_end, na.rm = T),
+#        decid = mean(prop_deciduous_end, na.rm = T),mixed = mean(prop_mixed_end, na.rm = T)), 
+#     by = .(int.year, jurisdiction)]
+# # snow, rock, barren, bryoids, generally below 1% availability
+# # dominant: needle, wetland
+# 
+# # aggregate habitat types
+# dat[, prop_mixforest_start := prop_deciduous_start + prop_mixed_start + prop_wettreed_start]
+# dat[, prop_veg_start := prop_shrub_start + prop_bryoids_start + prop_herbs_start]
+# dat[, prop_wets_start := prop_wetland_start]
+# dat[, prop_openbarren_start := prop_rock_start + prop_barren_start + prop_water_start]
+# dat[, prop_open_start := prop_rock_start + prop_barren_start + prop_bryoids_start + prop_herbs_start + prop_water_start]
+# dat[, prop_otherveg_start := prop_deciduous_start + prop_mixed_start + prop_wettreed_start +
+#       prop_shrub_start + prop_bryoids_start + prop_herbs_start]
+# 
+# dat[, prop_mixforest_end := prop_deciduous_end + prop_mixed_end + prop_wettreed_end]
+# dat[, prop_veg_end := prop_shrub_end + prop_bryoids_end + prop_herbs_end]
+# dat[, prop_wets_end := prop_wetland_end] 
+# dat[, prop_openbarren_end := prop_rock_end + prop_barren_end + prop_water_end]
+# dat[, prop_open_end := prop_rock_end + prop_barren_end + prop_bryoids_end + prop_herbs_end + prop_water_end]
+# dat[, prop_otherveg_end := prop_deciduous_end + prop_mixed_end + prop_wettreed_end +
+#       prop_shrub_end + prop_bryoids_end + prop_herbs_end]
+# 
+# # TODO should wettreed be in wets or mixforest?
+# # not include snow
+# 
+# ## this was for Canada LCC (landsat)
+# # dat[,.(needle =mean(prop_needleleaf_end, na.rm = T), decid = mean(prop_deciduous_end, na.rm = T),
+# #           mixed = mean(prop_mixed_end, na.rm = T), shrub = mean(prop_shrub_end, na.rm = T),
+# #           grass = mean(prop_grassland_end, na.rm = T), lichshrub = mean(prop_lichenshrub_end, na.rm = T),
+# #           lichgrass = mean(prop_lichengrass_end, na.rm = T), crop = mean(prop_cropland_end, na.rm = T),
+# #           wetland = mean(prop_wetland_end, na.rm = T), barren = mean(prop_barrenland_end, na.rm = T),
+# #           urban = mean(prop_urban_end, na.rm = T), water = mean(prop_water_end, na.rm = T),
+# #        snow = mean(prop_snow_end, na.rm = T)), by = .(int.year, jurisdiction)]
+# # not include urban or cropland
+# # dat[, prop_forest_start := prop_needleleaf_start + prop_deciduous_start + prop_mixed_start]
+# # dat[, prop_forage_start := prop_shrub_start + prop_grassland_start + prop_lichenshrub_start + prop_lichengrass_start]
+# # dat[, prop_open_start := prop_barrenland_start + prop_snow_start]
+# # dat[, prop_wets_start := prop_wetland_start + prop_water_start]
+# # 
+# # dat[, prop_forest_end := prop_needleleaf_end + prop_deciduous_end + prop_mixed_end]
+# # dat[, prop_forage_end := prop_shrub_end + prop_grassland_end + prop_lichenshrub_end + prop_lichengrass_end]
+# # dat[, prop_open_end := prop_barrenland_end + prop_snow_end]
+# # dat[, prop_wets_end := prop_wetland_end + prop_water_end]
+# 
+# dat[,.(needle =mean(prop_needleleaf_end, na.rm = T), mixforest =mean(prop_mixforest_end, na.rm = T), veg = mean(prop_veg_end, na.rm = T),
+#        open = mean(prop_open_end, na.rm = T), wets = mean(prop_wets_end, na.rm = T),
+#        openbarren = mean(prop_openbarren_end, na.rm = T), otherveg = mean(prop_otherveg_end, na.rm = T)), 
+#     by = .(int.year, jurisdiction)]
+# 
+# dat[,.(distlf_end =mean(distlf_end, na.rm = T), distlf_other_end = mean(distlf_other_end, na.rm = T),
+#        disturbance = mean(disturbance_end, na.rm = T), tsf = mean(ts_fires_end, na.rm = T),
+#        tsh = mean(ts_harv_end, na.rm = T)), 
+#     by = .(int.year, jurisdiction)]
+# ## prop_open too low availability for most
+# 
+# 
+# # change arbitrary time since variable
+# hist(dat[case_==TRUE]$ts_harv_end)
+# hist(dat[case_==FALSE]$ts_harv_end)
+# hist(dat[case_==TRUE & jurisdiction == 'mb']$ts_harv_end)
+# hist(dat[case_==FALSE & jurisdiction == 'mb']$ts_harv_end)
+# ggplot(data = dat, aes(case_, ts_harv_end, color = jurisdiction)) +
+#   geom_jitter() +
+#   geom_boxplot() +
+#   facet_wrap(~int.year)
+# 
+# hist(dat[case_==TRUE]$ts_fires_end)
+# hist(dat[case_==FALSE]$ts_fires_end)
+# hist(dat[case_==TRUE & jurisdiction == 'mb']$ts_fires_end)
+# hist(dat[case_==FALSE & jurisdiction == 'mb']$ts_fires_end)
+# ggplot(data = dat, aes(case_, ts_fires_end, color = jurisdiction)) +
+#   geom_jitter() +
+#   geom_boxplot() +
+#   facet_wrap(~int.year)
+# 
+# quantile(dat$ts_harv_end)
+# dat[ts_harv_start == 100, ts_harv_start:= 40]
+# dat[ts_harv_end == 100, ts_harv_end:= 40]
+# range(dat$ts_harv_end)
+# quantile(dat$ts_harv_end)
+# 
+# quantile(dat$ts_fires_end)
+# dat[ts_fires_start == 100, ts_fires_start:= 40]
+# dat[ts_fires_end == 100, ts_fires_end:= 40]
+# range(dat$ts_fires_end)
+# quantile(dat$ts_fires_end)
+# 
 
 range(dat$year)
 setindex(dat, NULL)
