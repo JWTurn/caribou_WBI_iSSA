6fc81b40d69f10b9422cbc33ec03d62687bd270e
Julie Turner <julwturner@gmail.com>
2023-10-02 17:25:23 -0600

    tweaks

--- scripts/4.2-iSSA_HPC.R
+++ scripts/4.2-iSSA_HPC.R
@@ -17,11 +17,11 @@ dat <- readRDS(file.path(derived, 'dat_iSSA.RDS'))
 
 
 range(dat$year)
-setindex(dat, NULL)
+#setindex(dat, NULL)
 # yr <- dat[case_==TRUE, .(year)]
 # hist(yr$year)
 dat.2010 <- dat[int.year==2010]
-dat.2015 <- dat[int.year==2015]
+#dat.2015 <- dat[int.year==2015]
 #dat.2020 <- dat[int.year==2020]
 
 dat.2010[,id:=as.factor(id)]
@@ -29,12 +29,12 @@ dat.2010[,indiv_step_id := as.factor(indiv_step_id)]
 dat.2010[,jurisdiction := as.factor(jurisdiction)]
 dat.2010[,year:=as.factor(year)]
 
-dat.2015[,id:=as.factor(id)]
-dat.2015[,indiv_step_id := as.factor(indiv_step_id)]
-dat.2015[,jurisdiction := as.factor(jurisdiction)]
-dat.2015[,year:=as.factor(year)]
+# dat.2015[,id:=as.factor(id)]
+# dat.2015[,indiv_step_id := as.factor(indiv_step_id)]
+# dat.2015[,jurisdiction := as.factor(jurisdiction)]
+# dat.2015[,year:=as.factor(year)]
 
-m2.2010 <- glmmTMB(case_ ~ -1 +
+m2 <- glmmTMB(case_ ~ -1 +
                      I(log(sl_+1)) +
                      I(cos(ta_)) +
                      I(log(sl_+1)):I(cos(ta_)) +
@@ -78,11 +78,11 @@ m2.2010 <- glmmTMB(case_ ~ -1 +
                      (0 + I(log(sl_+1)):I(log(distlf_other_start+1))|id) +
                      (0 + disturbance_end|id) +
                      (0 + I(log(sl_+1)):disturbance_start|id) +
-                     (1|jurisdiciton) + (1|year),
+                     (1|jurisdiction) + (1|year),
                    family = poisson(), data = dat.2010,
                    map= list(theta = factor(c(NA,1:23))),
                    start = list(theta =c(log(1000), seq(0,0, length.out = 23)))
 )
 
-summary(m2.2010)
+summary(m2)
 saveRDS(m2, file.path(derived, 'mod_sel_2010-2015.RDS'))
