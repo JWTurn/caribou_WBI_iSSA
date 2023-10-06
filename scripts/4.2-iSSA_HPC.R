739bc42f43aae9150bcf45c0b233568b4eb80ee9
Julie Turner <julwturner@gmail.com>
2023-10-06 13:57:59 -0600

    test hpc

--- scripts/4.2-iSSA_HPC.R
+++ scripts/4.2-iSSA_HPC.R
@@ -14,7 +14,9 @@ raw <- file.path('data', 'raw-data')
 derived <- file.path('data', 'derived-data')
 
 dat <- readRDS(file.path(derived, 'dat_iSSA.RDS'))
-
+# ggplot(dat[case_==TRUE], aes(sl_)) + 
+#   geom_density(aes(fill = jurisdiction), alpha = 0.3) 
+  
 #range(dat$year)
 #setindex(dat, NULL)
 # yr <- dat[case_==TRUE, .(year)]
@@ -22,17 +24,70 @@ dat <- readRDS(file.path(derived, 'dat_iSSA.RDS'))
 int.yr <- 2015
 juris <- 'mb'
 
-dat.yr<- dat[int.year==int.yr & jurisdiction == juris]
-#dat.yr<- dat[int.year==int.yr]
+#dat.yr<- dat[int.year==int.yr & jurisdiction == juris]
+dat.yr<- dat[int.year==int.yr]
 indivs <- sample(unique(dat.yr[jurisdiction == juris]$id), 
-                 ceiling(length(unique(dat.yr[jurisdiction == juris]$id))*.30))
+                 ceiling(length(unique(dat.yr[jurisdiction == juris]$id))*.80))
 dat.sub<- dat.yr[!(id %in% indivs)]
 
 dat.sub[,indiv_step_id := as.factor(indiv_step_id)]
-#dat.sub[,jurisdiction := as.factor(jurisdiction)]
+dat.sub[,jurisdiction := as.factor(jurisdiction)]
 dat.sub[,year:=as.factor(year)]
 
 print('prepped')
+
+# m <- glmmTMB(case_ ~ -1 +
+#                I(log(sl_+1)) +
+#                I(cos(ta_)) +
+#                I(log(sl_+1)):I(cos(ta_)) +
+#                prop_needleleaf_start:I(log(sl_+1)) +
+#                prop_mixforest_start:I(log(sl_+1)) +
+#                prop_veg_start:I(log(sl_+1)) +
+#                prop_wets_start:I(log(sl_+1)) +
+#                prop_needleleaf_end +
+#                prop_mixforest_end +
+#                prop_veg_end +
+#                prop_wets_end +
+#                I(log(ts_fires_end+1)) +
+#                I(log(sl_+1)):I(log(ts_fires_start+1)) +
+#                I(log(ts_harv_end+1)) +
+#                I(log(sl_+1)):I(log(ts_harv_start+1)) +
+#                I(log(distlf_end+1)) +
+#                I(log(sl_+1)):I(log(distlf_start+1)) +
+#                I(log(distlf_other_end+1)) +
+#                I(log(sl_+1)):I(log(distlf_other_start+1)) +
+#                disturbance_end +
+#                # I(log(sl_+1)):disturbance_start +
+#                (1|indiv_step_id) +
+#                (0 + I(log(sl_ +1))|jurisdiction/id) +
+#                (0 + I(cos(ta_))|jurisdiction/id) +
+#                (0 + I(log(sl_+1)):I(cos(ta_))|jurisdiction/id) +
+#                (0 + prop_needleleaf_start:I(log(sl_+1))|jurisdiction/id) +
+#                (0 + prop_mixforest_start:I(log(sl_+1))|jurisdiction/id) +
+#                (0 + prop_veg_start:I(log(sl_+1))|jurisdiction/id) +
+#                (0 + prop_wets_start:I(log(sl_+1))|jurisdiction/id) +
+#                (0 + prop_needleleaf_end|jurisdiction/id) +
+#                (0 + prop_mixforest_end|jurisdiction/id) +
+#                (0 + prop_veg_end|jurisdiction/id) +
+#                (0 + prop_wets_end|jurisdiction/id) +
+#                (0 + (I(log(ts_fires_end+1)))|jurisdiction/id) +
+#                (0 + I(log(sl_+1)):I(log(ts_fires_start+1))|jurisdiction/id) +
+#                (0 + (I(log(ts_harv_end+1)))|id) +
+#                (0 + I(log(sl_+1)):I(log(ts_harv_start+1))|jurisdiction/id) +
+#                (0 + I(log(distlf_end+1))|jurisdiction/id) +
+#                (0 + I(log(sl_+1)):I(log(distlf_start+1))|jurisdiction/id) +
+#                (0 + I(log(distlf_other_end+1))|jurisdiction/id) +
+#                (0 + I(log(sl_+1)):I(log(distlf_other_start+1))|jurisdiction/id) +
+#                (0 + disturbance_end|jurisdiction/id) #+
+#              # (0 + I(log(sl_+1)):disturbance_start|id) +
+#              # (1|jurisdiction) # +
+#              #(1|year)
+#              ,
+#              family = poisson(), data = dat.sub,
+#              map= list(theta = factor(c(NA,1:39))),
+#              start = list(theta =c(log(1000), seq(0,0, length.out = 39))),
+#              verbose = TRUE
+# )
 
 # m <- glmmTMB(case_ ~ -1 +
 #                       scale(I(log(sl_+1))) +
@@ -85,7 +140,7 @@ print('prepped')
 
 m <- glmmTMB(case_ ~ -1 +
                      I(log(sl_+1)) +
-                     I(cos(ta_)) +
+                    # I(cos(ta_)) +
                      I(log(sl_+1)):I(cos(ta_)) +
                      prop_needleleaf_start:I(log(sl_+1)) +
                      prop_mixforest_start:I(log(sl_+1)) +
@@ -96,18 +151,18 @@ m <- glmmTMB(case_ ~ -1 +
                      prop_veg_end +
                      prop_wets_end +
                      I(log(ts_fires_end+1)) +
-                     I(log(sl_+1)):I(log(ts_fires_start+1)) +
+                     #I(log(sl_+1)):I(log(ts_fires_start+1)) +
                      I(log(ts_harv_end+1)) +
-                     I(log(sl_+1)):I(log(ts_harv_start+1)) +
+                     #I(log(sl_+1)):I(log(ts_harv_start+1)) +
                      I(log(distlf_end+1)) +
-                     I(log(sl_+1)):I(log(distlf_start+1)) +
+                     #I(log(sl_+1)):I(log(distlf_start+1)) +
                      I(log(distlf_other_end+1)) +
-                     I(log(sl_+1)):I(log(distlf_other_start+1)) +
+                     #I(log(sl_+1)):I(log(distlf_other_start+1)) +
                      disturbance_end +
                     # I(log(sl_+1)):disturbance_start +
                      (1|indiv_step_id) +
                      (0 + I(log(sl_ +1))|id) +
-                     (0 + I(cos(ta_))|id) +
+                     #(0 + I(cos(ta_))|id) +
                      (0 + I(log(sl_+1)):I(cos(ta_))|id) +
                      (0 + prop_needleleaf_start:I(log(sl_+1))|id) +
                      (0 + prop_mixforest_start:I(log(sl_+1))|id) +
@@ -118,24 +173,24 @@ m <- glmmTMB(case_ ~ -1 +
                      (0 + prop_veg_end|id) +
                      (0 + prop_wets_end|id) +
                      (0 + (I(log(ts_fires_end+1)))|id) +
-                     (0 + I(log(sl_+1)):I(log(ts_fires_start+1))|id) +
+                     #(0 + I(log(sl_+1)):I(log(ts_fires_start+1))|id) +
                      (0 + (I(log(ts_harv_end+1)))|id) +
-                     (0 + I(log(sl_+1)):I(log(ts_harv_start+1))|id) +
+                     #(0 + I(log(sl_+1)):I(log(ts_harv_start+1))|id) +
                      (0 + I(log(distlf_end+1))|id) +
-                     (0 + I(log(sl_+1)):I(log(distlf_start+1))|id) +
+                     #(0 + I(log(sl_+1)):I(log(distlf_start+1))|id) +
                      (0 + I(log(distlf_other_end+1))|id) +
-                     (0 + I(log(sl_+1)):I(log(distlf_other_start+1))|id) +
+                     #(0 + I(log(sl_+1)):I(log(distlf_other_start+1))|id) +
                      (0 + disturbance_end|id) #+
                     # (0 + I(log(sl_+1)):disturbance_start|id) +
                     # (1|jurisdiction) # +
                     #(1|year)
              ,
                    family = poisson(), data = dat.sub,
-                   map= list(theta = factor(c(NA,1:20))),
-                   start = list(theta =c(log(1000), seq(0,0, length.out = 20))),
+                   map= list(theta = factor(c(NA,1:15))),
+                   start = list(theta =c(log(1000), seq(0,0, length.out = 15))),
                verbose = TRUE
     )
-
+#20
 summary(m)
 
 saveRDS(m, file.path(derived, paste0('mod_selmove_', juris, '_', int.yr, '-', int.yr+5,'_HPC.RDS')))
