1c0c8892d10a4ab878cdb82ac2eb1e57d704e187
Julie <julwturner@gmail.com>
2023-10-20 14:47:16 -0600

    update map making

--- scripts/5-maps.R
+++ scripts/5-maps.R
@@ -22,12 +22,6 @@ studyArea <- vect(file.path('data', 'derived-data', 'prepped-data', 'WBIprepDat_
 ab.range <- vect(file.path(canada, 'AB_CaribouSubregionalPlanBoundaries', 'CARIBOU_SUB_REGIONAL_PLAN_BOUNDARIES_2022_07_04.shp'))
 
 # load layers
-year = 2015
-linfeat_other <- rast(file.path('data', 'raw-data', 'ECCC_disturbance', 
-                                paste0('WB_lfother_', year, '_distto.tif')))
-disturb <- rast(file.path('data', 'raw-data', 'ECCC_disturbance', 
-                          paste0('WB_disturb_other_', year, '.tif')))
-
 
 yr = 2019
 
@@ -61,33 +55,13 @@ prop_mixforest <- deciduous + mixed + wettreed
 prop_veg <- shrub + bryoids + herbs
 prop_wets <- wet
 
-# needleleaf <- rast(file.path('data', 'raw-data','prop_land', year, '500grid',
-#                              paste0('needleleaf_500', '.tif')))
-# deciduous <- rast(file.path('data', 'raw-data','prop_land', year, '500grid',
-#                             paste0('deciduous_500', '.tif')))
-# mixed <- rast(file.path('data', 'raw-data','prop_land', year, '500grid',
-#                         paste0('mixed_500', '.tif')))
-# shrub <- rast(file.path('data', 'raw-data','prop_land', year, '500grid',
-#                         paste0('shrub_500', '.tif')))
-# grassland <- rast(file.path('data', 'raw-data','prop_land', year, '500grid',
-#                             paste0('grassland_500', '.tif')))
-# lichenshrub <- rast(file.path('data', 'raw-data','prop_land', year, '500grid',
-#                               paste0('lichenshrub_500', '.tif')))
-# lichengrass <- rast(file.path('data', 'raw-data','prop_land', year, '500grid',
-#                               paste0('lichengrass_500', '.tif')))
-# wetland <- rast(file.path('data', 'raw-data','prop_land', year, '500grid',
-#                           paste0('wet_500', '.tif')))
-# barrenland <- rast(file.path('data', 'raw-data','prop_land', year, '500grid',
-#                              paste0('barrenland_500', '.tif')))
-# water <- rast(file.path('data', 'raw-data','prop_land', year, '500grid',
-#                         paste0('water_500', '.tif')))
-# snow <- rast(file.path('data', 'raw-data','prop_land', year, '500grid',
-#                        paste0('snow_500', '.tif')))
-# 
-# prop_forest <- needleleaf + deciduous + mixed
-# prop_forage <- shrub + grassland + lichenshrub+ lichengrass
-# prop_open <- barrenland + snow
-# prop_wets <- wetland + water
+
+year = 2015
+linfeat_other <- rast(file.path('data', 'raw-data', 'ECCC_disturbance', 
+                                paste0('WB_lfother_', year, '_distto.tif')))
+disturb <- rast(file.path('data', 'raw-data', 'ECCC_disturbance', 
+                          paste0('WB_disturb_other_', year, '.tif')))
+
 
 
 fires <- rast(file.path('data', 'raw-data', 'fire_nbac_1986_to_2020', paste0('fires_', (year+5), '.tif')))
@@ -117,13 +91,6 @@ log_distlf <- log(lf + 1)
 log_distlfother <- log(lf_other.ext + 1)
 
 
-# removed open
-# props <- c(prop_forage, prop_forest, prop_wets, log_tsf)
-# props <- crop(props, ext(studyArea))
-# 
-# log_distlfother <- crop(log_distlfother, ext(studyArea))
-# disturb <- crop(disturb, ext(studyArea))
-
 land <- c(prop_veg, prop_needleleaf, prop_mixforest, prop_wets, log_tsf, log_tsh, log_distlf, 
           log_distlfother, disturb.ext)
 names(land) <- c('prop_veg', 'prop_needleleaf', 'prop_mixforest', 'prop_wets', 'log_tsf', 'log_tsh', 'log_distlf', 
@@ -131,7 +98,7 @@ names(land) <- c('prop_veg', 'prop_needleleaf', 'prop_mixforest', 'prop_wets', '
 
 
 
-# writeRaster(prop_forest.crop, file.path('data', 'derived-data', 'prop_forest_2015_500.tif'))
+# writeRaster(prop_forest.crop, file.path('data', 'derived-data', 'prop_forest_2010_500.tif'))
 # writeRaster(prop_forage.crop, file.path('data', 'derived-data', 'prop_forage_2015_500.tif'))
 # writeRaster(prop_open.crop, file.path('data', 'derived-data', 'prop_open_2015_500.tif'))
 # writeRaster(prop_wets.crop, file.path('data', 'derived-data', 'prop_wets_2015_500.tif'))
@@ -142,14 +109,27 @@ studyArea.ab <- union(studyArea, ab.range)
 
 
 ## Models ----
+sel.2010 <- readRDS(file.path(derived, 'mods_hpc', 'mod_selmove_2010-2015_HPC_noTA.RDS'))
 
 sel.2015 <- readRDS(file.path(derived, 'mods_hpc', 'mod_selmove_2015-2020_HPC_noTA.RDS'))
 
+sel.bc <- readRDS(file.path(derived, 'mod_selmove_bc.RDS'))
+sel.mb <- readRDS(file.path(derived, 'mod_selmove_mb_2015_50.RDS'))
+sel.sk <- readRDS(file.path(derived, 'mod_selmove_sk.RDS'))
+sel.nwt <- readRDS(file.path(derived, 'mod_selmove_nwt.RDS'))
+
 
 ### PDE ----
-View(summary(sel.2015)$coef$cond)
 
+mod2010re.tab <- make_betas_tab(sel.2010)
+
+View(summary(sel.2015)$coef$cond)
 mod2015re.tab <- make_betas_tab(sel.2015)
+
+bc.tab <- make_betas_tab(sel.bc)
+mb.tab <- make_betas_tab(sel.mb)
+sk.tab <- make_betas_tab(sel.sk)
+nwt.tab <- make_betas_tab(sel.nwt)
 
 
 gc()
@@ -159,92 +139,154 @@ canPoly <- project(canPoly, lf)
 wbi.prov <- subset(canPoly, canPoly$PREABBR %in% c('Alta.', 'B.C.', 'Man.', 'N.W.T.', 'Sask.', 'Y.T.'))
 
 
+bc <- subset(canPoly, canPoly$PREABBR %in% c('B.C.'))
+nwt <- subset(canPoly, canPoly$PREABBR %in% c('N.W.T.', 'Y.T.'))
+sk <- subset(canPoly, canPoly$PREABBR %in% c('Sask.'))
+mb <- subset(canPoly, canPoly$PREABBR %in% c('Man.'))
 
-#### full model PDE 2015 ----
+
+
+
+
+#### full model PDE functions ----
 gc()
-lf.cov.re.2015<- (2*as.double(mod2015re.tab[term %like% 'distlf_end', 
-                                            .(estimate)])*land$log_distlf)
-lfother.cov.re.2015<- (2*as.double(mod2015re.tab[term %like% 'distlf_other_end', 
-                                                 .(estimate)])*land$log_distlfother)
-tsf.cov.re.2015<- (2*as.double(mod2015re.tab[term %like% 'ts_fire', 
-                                             .(estimate)])*land$log_tsf)
-tsh.cov.re.2015<- (2*as.double(mod2015re.tab[term %like% 'ts_harv', 
-                                             .(estimate)])*land$log_tsh)
-needleleaf.cov.re.2015 <- (2*as.double(mod2015re.tab[term %like% 'needleleaf_end', 
-                                                 .(estimate)])*land$prop_needleleaf)
-veg.cov.re.2015 <- (2*as.double(mod2015re.tab[term %like% 'veg_end', 
-                                                 .(estimate)])*land$prop_veg)
-mixforest.cov.re.2015 <- (2*as.double(mod2015re.tab[term %like% 'mixforest_end',
-                                                .(estimate)])*land$prop_mixforest)
-wets.cov.re.2015 <- (2*as.double(mod2015re.tab[term %like% 'wets_end', 
-                                               .(estimate)])*land$prop_wets)
-disturb.cov.re.2015 <- (2*as.double(mod2015re.tab[term %like% 'disturb', 
-                                                  .(estimate)])*land$disturb)
+
+make_pde <- function(mod.tab, land){
+  lf.cov<- (2*as.double(mod.tab[term %like% 'distlf_end', 
+                                              .(estimate)])*land$log_distlf)
+  lfother.cov<- (2*as.double(mod.tab[term %like% 'distlf_other_end', 
+                                                   .(estimate)])*land$log_distlfother)
+  tsf.cov<- (2*as.double(mod.tab[term %like% 'ts_fires_end', 
+                                               .(estimate)])*land$log_tsf)
+  tsh.cov<- (2*as.double(mod.tab[term %like% 'ts_harv_end', 
+                                               .(estimate)])*land$log_tsh)
+  needleleaf.cov <- (2*as.double(mod.tab[term %like% 'needleleaf_end', 
+                                                       .(estimate)])*land$prop_needleleaf)
+  veg.cov <- (2*as.double(mod.tab[term %like% 'veg_end', 
+                                                .(estimate)])*land$prop_veg)
+  mixforest.cov <- (2*as.double(mod.tab[term %like% 'mixforest_end',
+                                                      .(estimate)])*land$prop_mixforest)
+  wets.cov <- (2*as.double(mod.tab[term %like% 'wets_end', 
+                                                 .(estimate)])*land$prop_wets)
+  disturb.cov <- (2*as.double(mod.tab[term %like% 'disturbance_end', 
+                                                    .(estimate)])*land$disturb)
+  
+  numerator <- exp(lf.cov + lfother.cov + 
+                             tsf.cov + tsh.cov + 
+                             needleleaf.cov + veg.cov + 
+                             mixforest.cov + 
+                             wets.cov +
+                             disturb.cov)
+  
+  
+  # the normalizing constant.
+  C <- global(numerator, sum, na.rm = T)
+  pde <- numerator/C[[1]]
+  
+  return(pde)
+}
 
 
-numerator.re.2015 <- exp(lf.cov.re.2015 + lfother.cov.re.2015 + 
-                           tsf.cov.re.2015 + tsh.cov.re.2015 + 
-                           needleleaf.cov.re.2015 + veg.cov.re.2015 + 
-                           mixforest.cov.re.2015 + 
-                           wets.cov.re.2015 +
-                           disturb.cov.re.2015)
 
-#plot(numerator,  breaks=seq(min(numerator, na.rm = T), max(numerator, na.rm = T), length.out = 10))
+make_pde_map <- function(pde, sArea, saveName = NULL){
+  pde.sa <- crop(pde, sArea, mask = T)
+  plot(pde.sa)
+  
+  breaks <- global(pde.sa, quantile, na.rm = T, probs = seq(0,1,.1))
+  v.breaks <- unname(breaks)
+  t.breaks <- as.vector(t(v.breaks))
+  pde.discrete <- classify(pde.sa, t.breaks, include.lowest=TRUE, brackets=TRUE)
+  
+  if(!is.null(saveName)){
+    writeRaster(pde.discrete, 
+                file.path(derived, saveName), overwrite = T)
+  }
+  
+  return(pde.discrete)
+}
 
-# the normalizing constant.
-C.re.2015 <- global(numerator.re.2015, sum, na.rm = T)
-pde.re.2015 <- numerator.re.2015/C.re.2015[[1]]
 
+### 2010 ----
 
-pde.re.2015.sa <- crop(pde.re.2015, studyArea, mask = T)
-plot(pde.re.2015.sa)
+pde.2010 <- make_pde(mod2010re.tab, land)
 
-breaks.re.2015 <- global(pde.re.2015.sa, quantile, na.rm = T, probs = seq(0,1,.1))
-v.breaks.re.2015 <- unname(breaks.re.2015)
-t.breaks.re.2015 <- as.vector(t(v.breaks.re.2015))
-pde.re.discrete.2015 <- classify(pde.re.2015.sa, t.breaks.re.2015, include.lowest=TRUE, brackets=TRUE)
-plot(pde.re.discrete.2015, breaks=0:10)
-
-writeRaster(pde.re.discrete.2015, 
-            file.path(derived, 'pde2015_re.tif'), overwrite = T)
+pde.2010.sa <- make_pde_map(pde.2010, studyArea, saveName = 'pde2010_re.tif')
+plot(pde.2010.sa, breaks=0:10)
 
 
 ## with ab guestimate
-pde.re.2015.sa.ab <- crop(pde.re.2015, studyArea.ab, mask = T)
+pde.2010.sa.ab <- make_pde_map(pde.2010, studyArea.ab, saveName = 'pde2010_re_ab.tif')
+plot(pde.2010.sa.ab, breaks=0:10)
 
-breaks.re.2015.sa.ab <- global(pde.re.2015.sa.ab, quantile, na.rm = T, probs = seq(0,1,.1))
-v.breaks.re.2015.sa.ab <- unname(breaks.re.2015.sa.ab)
-t.breaks.re.2015.sa.ab <- as.vector(t(v.breaks.re.2015.sa.ab))
-pde.re.discrete.2015.sa.ab <- classify(pde.re.2015.sa.ab, t.breaks.re.2015.sa.ab, include.lowest=TRUE, brackets=TRUE)
-plot(pde.re.discrete.2015.sa.ab, breaks=0:10)
+### 2015 ----
 
-writeRaster(pde.re.discrete.2015.sa.ab, 
-            file.path(derived, 'pde2015_re_ab.tif'))
+pde.2015 <- make_pde(mod2015re.tab, land)
 
+pde.2015.sa <- make_pde_map(pde.2015, studyArea)
+plot(pde.2015.sa, breaks=0:10)
 
 
+## with ab guestimate
+pde.2015.sa.ab <- make_pde_map(pde.2015, studyArea.ab, saveName = 'pde2015_re_ab.tif')
+plot(pde.2015.sa.ab, breaks=0:10)
 
 
+## Jurisidiction model pdes ----
+### BC ----
+pde.bc <- make_pde(bc.tab, land)
+bc.sa <- crop(studyArea, bc)
+pde.bc.sa <- make_pde_map(pde.bc, bc.sa, saveName = 'pde_bc.tif')
+
+### MB ----
+pde.mb <- make_pde(mb.tab, land)
+mb.sa <- crop(studyArea, mb)
+pde.mb.sa <- make_pde_map(pde.mb, mb.sa, saveName = 'pde_mb.tif')
+
+### SK ----
+pde.sk <- make_pde(sk.tab, land)
+sk.sa <- crop(studyArea, sk)
+pde.sk.sa <- make_pde_map(pde.sk, sk.sa, saveName = 'pde_sk.tif')
+
+### NWT + YT ----
+pde.nwt <- make_pde(nwt.tab, land)
+nwt.sa <- crop(studyArea, nwt)
+pde.nwt.sa <- make_pde_map(pde.nwt, nwt.sa, saveName = 'pde_nwt.tif')
 
 # plots ----
+pde.re.discrete.2010 <- rast(file.path(derived, 'pde2010_re.tif'))
+pde.re.discrete.2010.sa.ab <- rast(file.path(derived, 'pde2010_re_ab.tif'))
+
 pde.re.discrete.2015 <- rast(file.path(derived, 'pde2015_re.tif'))
 pde.re.discrete.2015.sa.ab <- rast(file.path(derived, 'pde2015_re_ab.tif'))
 
-# nwt <- subset(canPoly, canPoly$PREABBR %in% c('N.W.T.'))
-# nwt.2010 <- crop(pde.re.discrete.2010, nwt, mask = T)
-# nwt.2015 <- crop(pde.re.discrete.2015, nwt, mask = T)
-p.re.2015 <- ggplot(wbi.prov) +
+### 2010 plots ----
+p.re.2010.wbi<- ggplot(wbi.prov) +
   geom_spatvector(fill = NA) +
-  geom_spatraster(data = pde.re.2015.sa, show.legend = T) +
-  scale_fill_gradientn(colours = mako(10),na.value = NA) +
-  ggtitle('2015-2020 model') +
+  geom_spatraster(data = as.numeric(pde.re.discrete.2010), show.legend = T) +
+  scale_fill_gradientn(colours = mako(10),na.value = NA, limits = c(0,10)) +
+  ggtitle('2010-2015 model') +
   theme_bw() +
   theme(plot.title=element_text(size=12,hjust = 0.05),axis.title = element_blank()) +
   theme_void() +
   labs(fill = 'Intensity of selection') +
   coord_sf(crs = 3978)
-p.re.2015
+p.re.2010.wbi
 
+p.re.2010.wbi.ab <- ggplot(wbi.prov) +
+  geom_spatvector(fill = NA) +
+  geom_spatraster(data = as.numeric(pde.re.discrete.2010.sa.ab), show.legend = T) +
+  scale_fill_gradientn(colours = mako(10),na.value = NA, limits = c(0,10)) +
+  ggtitle('2010-2015 model extrapolating AB') +
+  theme_bw() +
+  theme(plot.title=element_text(size=12,hjust = 0.05),axis.title = element_blank()) +
+  theme_void() +
+  labs(fill = 'Intensity of selection') +
+  coord_sf(crs = 3978)
+p.re.2010.wbi.ab
+
+p.re.2010.wbi + p.re.2010.wbi.ab
+
+### 2015 plots ----
 p.re.2015.wbi<- ggplot(wbi.prov) +
   geom_spatvector(fill = NA) +
   geom_spatraster(data = as.numeric(pde.re.discrete.2015), show.legend = T) +
@@ -269,9 +311,77 @@ p.re.2015.wbi.ab <- ggplot(wbi.prov) +
   coord_sf(crs = 3978)
 p.re.2015.wbi.ab
 
-p.re.2010.wbi + p.re.2010.wbi.ab
+p.re.2015.wbi + p.re.2015.wbi.ab
 
+### BC plot ----
+p.bc <- ggplot(bc) +
+  geom_spatvector(fill = NA) +
+  geom_spatraster(data = as.numeric(pde.bc.sa), show.legend = T) +
+  scale_fill_gradientn(colours = mako(10),na.value = NA, limits = c(0,10)) +
+  ggtitle('BC') +
+  theme_bw() +
+  theme(plot.title=element_text(size=12,hjust = 0.05),axis.title = element_blank()) +
+  theme_void() +
+  labs(fill = 'Intensity of selection') +
+  coord_sf(crs = 3978)
+p.bc
 
+### MB plot ----
+p.mb <- ggplot(mb) +
+  geom_spatvector(fill = NA) +
+  geom_spatraster(data = as.numeric(pde.mb.sa), show.legend = T) +
+  scale_fill_gradientn(colours = mako(10),na.value = NA, limits = c(0,10)) +
+  ggtitle('mb') +
+  theme_bw() +
+  theme(plot.title=element_text(size=12,hjust = 0.05),axis.title = element_blank()) +
+  theme_void() +
+  labs(fill = 'Intensity of selection') +
+  coord_sf(crs = 3978)
+p.mb
+
+### SK plot ----
+p.sk <- ggplot(sk) +
+  geom_spatvector(fill = NA) +
+  geom_spatraster(data = as.numeric(pde.sk.sa), show.legend = T) +
+  scale_fill_gradientn(colours = mako(10),na.value = NA, limits = c(0,10)) +
+  ggtitle('sk') +
+  theme_bw() +
+  theme(plot.title=element_text(size=12,hjust = 0.05),axis.title = element_blank()) +
+  theme_void() +
+  labs(fill = 'Intensity of selection') +
+  coord_sf(crs = 3978)
+p.sk
+
+### NWT plot ----
+p.nwt <- ggplot(nwt) +
+  geom_spatvector(fill = NA) +
+  geom_spatraster(data = as.numeric(pde.nwt.sa), show.legend = T) +
+  scale_fill_gradientn(colours = mako(10),na.value = NA, limits = c(0,10)) +
+  ggtitle('nwt') +
+  theme_bw() +
+  theme(plot.title=element_text(size=12,hjust = 0.05),axis.title = element_blank()) +
+  theme_void() +
+  labs(fill = 'Intensity of selection') +
+  coord_sf(crs = 3978)
+p.nwt
+
+### BC plot ----
+p.juris <- ggplot(wbi.prov) +
+  geom_spatvector(fill = NA) +
+  geom_spatraster(data = as.numeric(pde.bc.sa), show.legend = T) +
+  geom_spatraster(data = as.numeric(pde.nwt.sa), show.legend = F) +
+  geom_spatraster(data = as.numeric(pde.mb.sa), show.legend = F) +
+  geom_spatraster(data = as.numeric(pde.sk.sa), show.legend = F) +
+  scale_fill_gradientn(colours = magma(10),na.value = NA, limits = c(0,10)) +
+  ggtitle('Jurisdictional models') +
+  theme_bw() +
+  theme(plot.title=element_text(size=12,hjust = 0.05),axis.title = element_blank()) +
+  theme_void() +
+  labs(fill = 'Intensity of selection') +
+  coord_sf(crs = 3978)
+p.juris
+
+#######
 ###### nwt ----
 nwt.sf <- st_as_sf(nwt)
 nwt.2010.rast <- raster::raster(nwt.2010)
